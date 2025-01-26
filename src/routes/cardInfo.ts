939e77f28d92a7597fe692824dc4b9f3f0fc21e8
zaksiu12s <zaksiu279@gmail.com>
2025-01-26 10:44:51 +0100

    1.1.1 simplify specified routes

--- src/routes/cardInfo.ts
+++ src/routes/cardInfo.ts
@@ -2,45 +2,13 @@ import express from "express";
 import getCardInfo from "../controller/cardInfo/getCardInfo";
 import createCard from "../controller/cardInfo/createCard";
 import getAllCardInfo from "../controller/cardInfo/getAllCardInfo";
-import { Request, Response } from "express";
-import CardInfo, { CardInfoType } from "../models/CardInfo";
+import getMainCardInfo from "../controller/cardInfo/getMainCardInfo";
 
 const router = express.Router();
 
 router.get("/", getAllCardInfo);
-router.get("/main", async (_: Request, res: Response) => {
-  try {
-    const cardInfo = await CardInfo.findOne<CardInfoType>({
-      isMain: true,
-    });
-
-    if (!cardInfo) {
-      res.sendStatus(404);
-      return;
-    }
-
-    const responseData = {
-      information: cardInfo.information,
-      design: cardInfo.design,
-      id: cardInfo._id,
-      isMain: cardInfo.isMain,
-      fields: cardInfo.fields.map((field) => {
-        return {
-          label: field.label,
-          value: field.value,
-        };
-      }),
-    };
-
-    res.status(200).json({ ...responseData });
-    return;
-  } catch (err) {
-    console.log(err);
-    res.sendStatus(500);
-    return;
-  }
-});
+router.get("/main", getMainCardInfo);
 router.get("/:id", getCardInfo);
-router.post("/create", createCard);
+router.post("/", createCard);
 
 export default router;
