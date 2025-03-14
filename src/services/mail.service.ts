13f53d56faf0e7e192094065ac997992f1d440dd
Maksymilian Zając <zaksiu279@gmail.com>
2025-03-14 13:52:13 +0100

    3.5.0 add development and production mode for mail sending and transporter settings

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -3,17 +3,19 @@ import { transporter } from "../config/nodemailer";
 import { UserDocument } from "../models/User.model";
 import { createUserLog } from "./auth.service";
 import { UserLogType } from "../constants/userLogTypes";
+import { ENVIRONMENT } from "../constants/env";
 
 export async function sendVerificationMail(
   user: UserDocument,
   code: mongoose.Types.ObjectId
 ) {
-  try {
-    let info = await transporter.sendMail({
-      from: '"Cardify" <zajacmaksymilian@icloud.com>',
-      to: user.email,
-      subject: "Cardify - Account verification",
-      text: `
+  if (ENVIRONMENT === "production") {
+    try {
+      let info = await transporter.sendMail({
+        from: '"Cardify" <zajacmaksymilian@icloud.com>',
+        to: user.email,
+        subject: "Cardify - Account verification",
+        text: `
         Hello, ${user.firstName} ${user.lastName}!
 
         Please verify your account by clicking on the link below:
@@ -22,12 +24,15 @@ export async function sendVerificationMail(
 
         Best regards,
       `,
-    });
-    console.log("E-mail sent:", info.messageId);
-    await createUserLog(user, UserLogType.EmailSent, "Email sent");
-    return info.messageId;
-  } catch (error) {
-    console.error("Error sending email:", error);
-    return null;
+      });
+      console.log("E-mail sent:", info.messageId);
+      await createUserLog(user, UserLogType.EmailSent, "Email sent");
+      return info.messageId;
+    } catch (error) {
+      console.error("Error sending email:", error);
+      return null;
+    }
   }
+
+  return null;
 }
