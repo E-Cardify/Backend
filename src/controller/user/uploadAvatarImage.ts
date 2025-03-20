726dbb3e5584e1c733a8d08690161605713971b5
Maksymilian Zając <zaksiu279@gmail.com>
2025-03-20 22:31:07 +0100

    3.6.1 divide avatar upload code into services

--- src/controller/user/uploadAvatarImage.ts
+++ src/controller/user/uploadAvatarImage.ts
@@ -1,56 +1,14 @@
 import { Request, Response } from "express";
-import UserModel from "../../models/User.model";
-import { BAD_REQUEST, NOT_FOUND, OK } from "../../constants/http";
-import appAssert from "../../utils/appAssert";
+import { uploadAvatarImageHandler } from "../../services/user.service";
+import { BAD_REQUEST, OK } from "../../constants/http";
 import { formatUserPrivateDataResponse } from "../../utils/responseUtils";
-import {
-  deleteFromCloudinary,
-  uploadToCloudinary,
-} from "../../services/cloudinary.service";
-import { imageExtensions } from "../../constants/fileExtensions";
-import validateImageFileExtension from "../../utils/validateFileExtensions";
-import FileModel from "../../models/File.model";
+import appAssert from "../../utils/appAssert";
 
 export const uploadAvatarImage = async (req: Request, res: Response) => {
-  const user = await UserModel.findById(req.userId);
-  appAssert(user, NOT_FOUND, "User not found");
-  appAssert(req.file, BAD_REQUEST, "Avatar image is missing");
+  const { userId, file } = req;
+  appAssert(file, BAD_REQUEST, "Avatar image is missing");
 
-  const { avatarPublicId } = user;
-
-  if (avatarPublicId) {
-    await deleteFromCloudinary(avatarPublicId);
-    await FileModel.deleteOne({
-      publicId: avatarPublicId,
-      userId: user._id!.toString(),
-    });
-
-    user.avatarUrl = "";
-    user.avatarPublicId = "";
-  }
-
-  const isExtensionValid = validateImageFileExtension(req.file);
-  appAssert(
-    isExtensionValid,
-    BAD_REQUEST,
-    `Invalid file extension, supported: ${imageExtensions.join(", ")}`
-  );
-
-  const { url, publicId } = await uploadToCloudinary(req.file.buffer);
-
-  const file = new FileModel({
-    publicId: publicId,
-    url: url,
-    userId: user._id,
-    public: true,
-    type: "image",
-  });
-
-  user.avatarUrl = url;
-  user.avatarPublicId = publicId;
-
-  await user.save();
-  await file.save();
+  const { user } = await uploadAvatarImageHandler(userId, file);
 
   res.status(OK).json(formatUserPrivateDataResponse(user));
 };
---
+++ src/services/user.service.ts
@@ -0,0 +1,65 @@
+import { Types } from "mongoose";
+import UserModel from "../models/User.model";
+import appAssert from "../utils/appAssert";
+import { BAD_REQUEST, NOT_FOUND } from "../constants/http";
+import { deleteFromCloudinary, uploadToCloudinary } from "./cloudinary.service";
+import FileModel from "../models/File.model";
+import validateImageFileExtension from "../utils/validateFileExtensions";
+import { imageExtensions } from "../constants/fileExtensions";
+import { createUserLog } from "./auth.service";
+import { UserLogType } from "../constants/userLogTypes";
+
+async function uploadAvatarImageHandler(
+  userId: Types.ObjectId,
+  file: Express.Multer.File
+) {
+  const user = await UserModel.findById(userId);
+  appAssert(user, NOT_FOUND, "User not found");
+
+  const { avatarPublicId } = user;
+
+  if (avatarPublicId) {
+    await deleteFromCloudinary(avatarPublicId);
+    await FileModel.deleteOne({
+      publicId: avatarPublicId,
+      userId: user._id!.toString(),
+    });
+
+    user.avatarUrl = "";
+    user.avatarPublicId = "";
+  }
+
+  const isExtensionValid = validateImageFileExtension(file);
+  appAssert(
+    isExtensionValid,
+    BAD_REQUEST,
+    `Invalid file extension, supported: ${imageExtensions.join(", ")}`
+  );
+
+  const { url, publicId } = await uploadToCloudinary(file.buffer);
+
+  const newFile = new FileModel({
+    publicId: publicId,
+    url: url,
+    userId: user._id,
+    public: true,
+    type: "image",
+  });
+
+  user.avatarUrl = url;
+  user.avatarPublicId = publicId;
+
+  createUserLog(
+    user,
+    UserLogType.AccountUpdated,
+    "Avatar image changed",
+    false
+  );
+
+  await user.save();
+  await newFile.save();
+
+  return { user, file: newFile };
+}
+
+export { uploadAvatarImageHandler };
